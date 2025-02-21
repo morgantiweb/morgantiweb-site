0a85ae4390e377ce306c9b660083c93acf2a9ba4
MorgantiWeb <info@morgantiweb.com>
2025-02-21 02:24:09 +0100

    timeline

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,13 +2,13 @@ import SectionCover from "./sections/SectionCover";
 import SectionImageCarousel from "./sections/SectionImageCarousel";
 import SectionWorks from "./sections/SectionWorks";
 
-
 function Container() {
   return (
     <>
       <SectionCover />
       <SectionImageCarousel />
       <SectionWorks />
+      <div style={{ height: "100vh" }}>to be continue...</div>
     </>
   );
 }
--- src/components/sections/SectionWorks.jsx
+++ src/components/sections/SectionWorks.jsx
@@ -1,5 +1,6 @@
 import { useTranslation } from "react-i18next";
 import LazyImage from "../LazyImage";
+import { useEffect, useRef, useState } from "react";
 
 const works = [
   {
@@ -40,7 +41,7 @@ const works = [
       "Jest",
       "SCSS",
       "Webpack",
-      "PWA"
+      "PWA",
     ],
     companyLink: "https://www.ynap.com/",
   },
@@ -92,14 +93,7 @@ const works = [
     companyName: "CEDAC Software Srl",
     title: "cedac-jobtitle",
     description: "cedac-jobdesc",
-    tecnologies: [
-      "Java",
-      "HTML5",
-      "XML",
-      "XSLT",
-      "CSS",
-      "Javascript",
-    ],
+    tecnologies: ["Java", "HTML5", "XML", "XSLT", "CSS", "Javascript"],
     companyLink: "https://www.cedac.com/",
   },
   {
@@ -117,85 +111,165 @@ const works = [
       "Adobe Flash",
       "Adobe Dreamweaver",
     ],
-    companyLink: "https://www.gamesurf.it/recensioni/gioco/pc-basket-2003-c6738",
+    companyLink:
+      "https://www.gamesurf.it/recensioni/gioco/pc-basket-2003-c6738",
   },
 ];
 
 export default function SectionWorks() {
   const { i18n, t } = useTranslation();
 
+  const [activeSection, setActiveSection] = useState(false);
+  const [activeYear, setActiveYear] = useState(null);
+  const sectionRef = useRef();
+  const sectionWorksRef = useRef([]);
+
+  const years = [];
+  for (
+    let y = new Date().getFullYear();
+    y >= works[works.length - 1].dateStart.getFullYear() - 1;
+    y--
+  ) {
+    years.push(y);
+  }
+
+  const workYears = [
+    new Date().getFullYear(),
+    ...new Set(works.map((w) => w.dateStart.getFullYear())),
+  ];
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const year = entry.target.getAttribute("data-year");
+            setActiveYear(year);
+          }
+        });
+      },
+      { threshold: 0.50 }
+    );
+
+    const sectionObserver = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveSection(true);
+          } else {
+            setActiveSection(false);
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    sectionObserver.observe(sectionRef.current);
+
+    sectionWorksRef.current.forEach((section) => {
+      if (section) observer.observe(section);
+    });
+
+    return () => {
+      sectionWorksRef.current.forEach((section) => {
+        if (section) observer.unobserve(section);
+      });
+    };
+  }, []);
+
   return (
-    <section className="px-8 py-16 pt-32">
-      <div className="container max-w-screen-xl mx-auto">
-        <h2 className="title-h2 mb-8">{t("work-experience")}</h2>
-        <div className="space-y-24">
-          <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-            {works.map((w) => (
-              <li className="py-12">
-                <article>
-                  <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-top xl:space-y-0">
-                    <div>
-                      <LazyImage
-                        fileName={`companies/${w.companyLogo}`}
-                        width={75}
-                        srcSet={["w=150&dpr=1 1x,", "w=200&dpr=2 2x,"]}
-                        alt={w.companyName}
-                        className="company"
-                      />
-                      <h4 className="title-h4">{w.companyName}</h4>
-                      <dl>
-                        <dt className="sr-only">Duration</dt>
-                        <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
-                          <time dateTime={w.dateStart.toISOString()}>
-                            {w.dateStart.toLocaleDateString(i18n.language, {
-                              year: "numeric",
-                              month: "long",
-                            })}
-                          </time>
-                          -
-                          {w.dateEnd ? (
+    <>
+      <div className={`timeline-container ${activeSection ? "active" : ""}`}>
+        {years.map((year, index) =>
+          workYears.includes(year) ? (
+            <div
+              key={"year-" + index}
+              className={`timeline-work ${activeYear == year ? "active" : ""}`}
+            >
+              {year}
+            </div>
+          ) : (
+            <div
+              key={"year-" + index}
+              className={`timeline-dot ${activeYear == year ? "active" : ""}`}
+            ></div>
+          )
+        )}
+      </div>
+      <section className="px-8 py-16 pt-32" ref={sectionRef}>
+        <div className="container max-w-screen-xl mx-auto">
+          <h2 className="title-h2 mb-8">{t("work-experience")}</h2>
+          <div className="space-y-24">
+            <ul className="divide-y divide-gray-200 dark:divide-gray-700">
+              {works.map((w, i) => (
+                <li key={"work-" + i} className="py-12" data-year={w.dateStart.getFullYear()} ref={(el) => (sectionWorksRef.current[i] = el)}>
+                  <article>
+                    <div className="space-y-2 xl:grid xl:grid-cols-4 xl:items-top xl:space-y-0 pr-7">
+                      <div>
+                        <LazyImage
+                          fileName={`companies/${w.companyLogo}`}
+                          width={75}
+                          srcSet={["w=150&dpr=1 1x,", "w=200&dpr=2 2x,"]}
+                          alt={w.companyName}
+                          className="company"
+                        />
+                        <h4 className="title-h4">{w.companyName}</h4>
+                        <dl>
+                          <dt className="sr-only">Duration</dt>
+                          <dd className="text-base leading-6 font-medium text-gray-500 dark:text-gray-400">
                             <time dateTime={w.dateStart.toISOString()}>
-                              {w.dateEnd.toLocaleDateString(i18n.language, {
+                              {w.dateStart.toLocaleDateString(i18n.language, {
                                 year: "numeric",
                                 month: "long",
                               })}
                             </time>
-                          ) : (
-                            t("present")
-                          )}
-                        </dd>
-                      </dl>
-                    </div>
-                    <div className="space-y-5 xl:col-span-3">
-                      <div className="space-y-6">
-                        <div>
-                          <h3 className="title-h3 mb-2">{t(w.title)}</h3>
-                          <div className="flex flex-wrap">
-                            {w.tecnologies.map((t) => (
-                              <span className="tecnology-tag">{t}</span>
-                            ))}
+                            -
+                            {w.dateEnd ? (
+                              <time dateTime={w.dateStart.toISOString()}>
+                                {w.dateEnd.toLocaleDateString(i18n.language, {
+                                  year: "numeric",
+                                  month: "long",
+                                })}
+                              </time>
+                            ) : (
+                              t("present")
+                            )}
+                          </dd>
+                        </dl>
+                      </div>
+                      <div className="space-y-5 xl:col-span-3">
+                        <div className="space-y-6">
+                          <div>
+                            <h3 className="title-h3 mb-2">{t(w.title)}</h3>
+                            <div className="flex flex-wrap">
+                              {w.tecnologies.map((t, i) => (
+                                <span key={i} className="tecnology-tag">
+                                  {t}
+                                </span>
+                              ))}
+                            </div>
                           </div>
+                          <div className="paragraph">{t(w.description)}</div>
                         </div>
-                        <div className="paragraph">{t(w.description)}</div>
-                      </div>
-                      <div className="text-base leading-6 font-medium">
-                        <a
-                          className="link"
-                          aria-label={`${t("read-more")}: "${"title"}"`}
-                          href={w.companyLink}
-                          target="_blank"
-                        >
-                          {t("read-more")} →
-                        </a>
+                        <div className="text-base leading-6 font-medium">
+                          <a
+                            className="link"
+                            aria-label={`${t("read-more")}: "${"title"}"`}
+                            href={w.companyLink}
+                            target="_blank"
+                          >
+                            {t("read-more")} →
+                          </a>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </article>
-              </li>
-            ))}
-          </ul>
+                  </article>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
